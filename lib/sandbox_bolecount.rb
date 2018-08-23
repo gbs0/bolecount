e7d43ea295b13b4cf24fe785687d9d15f5f3c7b6
gbs0 <gabriel@lewagon.org>
2018-08-23 13:09:23 -0300

    Fixed bolecount script

--- lib/sandbox_bolecount.rb
+++ lib/sandbox_bolecount.rb
@@ -15,8 +15,7 @@ class Bolecount
   end
 
   def ask_final
-  $stdout.puts 'Qual o valor pago? (ou \'sair\' para sair)'
-  n = gets.chomp.to_f
+      
   end
 
   #Método que recebe o algoritmo de soma
@@ -25,7 +24,7 @@ class Bolecount
     @cache ||= {}
     return @cache[n] if @cache[n]
     
-    ALLOWED_NUMBERS.each do |x|  
+    @boletos.each do |x|  
       next if (n + 1) - x < 0
       lesser_sum = into_sum_of_allowed_numbers(n - x)
       next unless lesser_sum
@@ -39,8 +38,6 @@ class Bolecount
     $stdout.puts 'Digite o valor do boleto: (ou \'ok\' para continuar)'
     b = gets.chomp
     if b == 'ok'
-      ask_final
-      into_sum_of_allowed_numbers(n)
       show_result
     else
       @boletos << b.to_f
@@ -48,19 +45,13 @@ class Bolecount
     end
   end
 
-  def show_boletos
-    #Mostrando os valores no terminal
-    $stdout.puts
-    $stdout.puts 'Tabela dos valores inseridos:'
-    $stdout.puts @boletos.sort.to_s
-    $stdout.puts 'Total:R$ ' + calculating_values.to_s
-    $stdout.puts '-' * 34
-    $stdout.puts
-  end
-
   #Mostrando os números
   #into_sum_of_allowed_numbers(4.5) # ==> [0.5, 2, 2]
   def show_result
+  $stdout.puts 'Qual o valor pago? (ou \'sair\' para sair)'
+  n = gets.chomp.to_f
+  show_boletos
+
   $stdout.puts 
   $stdout.puts '-' * 34
   $stdout.puts 'Esses são os valores que correspondem sua soma:'
@@ -68,6 +59,18 @@ class Bolecount
   $stdout.puts '-' * 34
   $stdout.puts
   end
+
+  def show_boletos
+    #Mostrando os valores no terminal
+    $stdout.puts
+    $stdout.puts '-' * 34
+    $stdout.puts 'Tabela dos valores inseridos:'
+    $stdout.puts @boletos.sort.to_s
+    $stdout.puts 'Total:R$ ' + calculating_values.to_s
+    $stdout.puts '-' * 34
+    $stdout.puts
+  end
+
 end
 
 bolecount = Bolecount.new
