4a4d96a1f1d2538d2980e735ef04da6ab5e914bd
gbs0 <gabriel@lewagon.org>
2018-08-24 08:36:24 -0300

    UI improvements and some minor bug fixes

--- lib/sandbox_bolecount.rb
+++ lib/sandbox_bolecount.rb
@@ -14,9 +14,6 @@ class Bolecount
     return int
   end
 
-  def ask_final
-      
-  end
 
   #Método que recebe o algoritmo de soma
   def into_sum_of_allowed_numbers(n)
@@ -35,10 +32,14 @@ class Bolecount
   end
 
   def ask_for_new_boleto
-    $stdout.puts 'Digite o valor do boleto: (ou \'ok\' para continuar)'
+    $stdout.puts 'Digite o valor dos boletos: (\'ok\' p/ entrar valor pago ou \'h\' p/ ajuda)'
     b = gets.chomp
     if b == 'ok'
       show_result
+    elsif b == 'h'
+      show_help
+    elsif b == 'sair'
+      exit!      
     else
       @boletos << b.to_f
       show_boletos
@@ -47,9 +48,14 @@ class Bolecount
 
   #Mostrando os números
   #into_sum_of_allowed_numbers(4.5) # ==> [0.5, 2, 2]
+  def show_help
+    $stdout.puts '(Utilize \'CTRL+C\' p/ sair)'
+    $stdout.puts '(Dica: Use \'.\' p/ separar as casas decimais)'
+  end
+
   def show_result
   $stdout.puts 'Qual o valor pago? (ou \'sair\' para sair)'
-  n = gets.chomp.tso_f
+  n = gets.chomp.to_f
   show_boletos
 
   $stdout.puts 
